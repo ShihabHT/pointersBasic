762e9c53b87b42a9cefa1962421cbeaa460cd235
ShihabHT <shihabrahman122@gmail.com>
2022-08-16 20:45:57 +0600

    message added

--- multiVariableReturnFunc/multiVariableReturnFunc.cpp
+++ multiVariableReturnFunc/multiVariableReturnFunc.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-void root(int a, int b, int c, double& r1, double& r2){
-
+void root(int a, int b, int c, double& r1, double& r2){ //this way we can take the reference of a variable directly without creating a pointer variable(ex. int& a)
     double discriminant;
     discriminant = b*b-4*a*c;
     if(discriminant<0){
@@ -18,7 +17,7 @@ void root(int a, int b, int c, double& r1, double& r2){
 
 int main(){
     double r1, r2;
-    root(1, -5, 6, r1, r2);
+    root(1, -5, 6, r1, r2); //passing the variable r1, r2 directly to create its reference in the function
     cout << r1<<" "<<r2 << endl;
     return 0;
 }
